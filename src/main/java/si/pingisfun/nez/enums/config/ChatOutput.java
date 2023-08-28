218b80bcd83f2ac01f0acdb0bd40015af3aeca81
PingIsFun <pingisfun@protonmail.com>
2023-08-28 22:51:22 +0200

    Add ChatOutputEnum

---
+++ src/main/java/si/pingisfun/nez/enums/config/ChatOutput.java
@@ -0,0 +1,33 @@
+package si.pingisfun.nez.enums.config;
+
+public enum ChatOutput {
+    OFF("Off"),
+    SELF("Self"),
+    PARTY("Party"),
+    CHAT("Chat");
+
+    private final String chatTypeString;
+
+    ChatOutput(String chatTypeString) {
+        this.chatTypeString = chatTypeString;
+
+    }
+
+    public static ChatOutput getOutputUpByName(String name) {
+        for (ChatOutput chatOutput : ChatOutput.values()) {
+            if (chatOutput.chatTypeString.equalsIgnoreCase(name)) {
+                return chatOutput;
+            }
+        }
+        return ChatOutput.OFF;
+    }
+    public static ChatOutput getOutputUpByNumber(int num) {
+        ChatOutput[] values = ChatOutput.values();
+        if (values.length <= num || num < 0) {
+            return ChatOutput.OFF;
+        }
+        return values[num];
+    }
+
+
+}
