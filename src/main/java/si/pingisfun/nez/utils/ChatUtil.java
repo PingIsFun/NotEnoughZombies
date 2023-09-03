e562298cd19d3fed4c228aafab7ecf541ded60f7
PingIsFun <pingisfun@protonmail.com>
2023-09-03 21:50:54 +0200

    Add sendLuckyChestRollDataToChat()

--- src/main/java/si/pingisfun/nez/utils/ChatUtil.java
+++ src/main/java/si/pingisfun/nez/utils/ChatUtil.java
@@ -7,6 +7,8 @@ import si.pingisfun.nez.NotEnoughZombies;
 import si.pingisfun.nez.config.ModConfig;
 import si.pingisfun.nez.enums.config.ChatOutput;
 
+import java.util.Map;
+
 public class ChatUtil {
     static final String PREFIX = ChatColor.DARK_GREEN + "[" + ChatColor.GOLD + NotEnoughZombies.NAME + ChatColor.DARK_GREEN + "] " + ChatColor.RESET;
     static final String SHORT_PREFIX = ChatColor.DARK_GREEN + "[" + ChatColor.GOLD + NotEnoughZombies.MODID.toUpperCase() + ChatColor.DARK_GREEN + "] " + ChatColor.RESET;
@@ -60,4 +62,32 @@ public class ChatUtil {
         Minecraft.getMinecraft().thePlayer.sendChatMessage(message);
     }
 
+
+    public static void sendLuckyChestRollDataToChat() {
+        ChatUtil.message("Lucky Chest Roll Data:");
+        for (Map.Entry<String, Map<String, Integer>> outerEntry : NotEnoughZombies.game.getLuckyChestRollData().entrySet()) {
+            StringBuilder result = new StringBuilder();
+
+            // Indentation based on the level
+            result.append(ChatColor.GOLD).append(outerEntry.getKey()).append(": ");
+
+            Map<String, Integer> innerMap = outerEntry.getValue();
+            int c = 0;
+            for (Map.Entry<String, Integer> innerEntry : innerMap.entrySet()) {
+                // Indentation for subentries
+                result.append(ChatColor.WHITE)
+                        .append(innerEntry.getKey())
+                        .append(": ")
+                        .append(innerEntry.getValue());
+
+                if (c >= innerMap.size() - 1) {
+                    break;
+                }
+                result.append(", ");
+
+                c++;
+            }
+            ChatUtil.message(result.toString(), ChatOutput.SELF);
+        }
+    }
 }
