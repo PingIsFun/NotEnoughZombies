9df266212b82295e67c2d7e7795be12d318506b3
PingIsFun <pingisfun@protonmail.com>
2023-09-22 12:06:05 +0200

    Replace LOGGER with DEBUG_LOGGER in debugScoreboard()

--- src/main/java/si/pingisfun/nez/utils/DevUtils.java
+++ src/main/java/si/pingisfun/nez/utils/DevUtils.java
@@ -10,14 +10,15 @@ import static si.pingisfun.nez.utils.MinecraftUtils.getSidebarLines;
 
 public class DevUtils {
     public static void debugScoreboard() {
+        NotEnoughZombies.DEBUG_LOGGER.info("Scoreboard debug:");
         Scoreboard scoreboard = Minecraft.getMinecraft().thePlayer.getWorldScoreboard();
-        NotEnoughZombies.LOGGER.info(scoreboard);
+        NotEnoughZombies.DEBUG_LOGGER.info(scoreboard);
         List<String> lines = getSidebarLines();
         for (int i = 0; i < lines.size(); i++) {
             String rawLine = lines.get(i);
             String line = MinecraftUtils.removeFormatting(rawLine);
-            NotEnoughZombies.LOGGER.info(i + " " + rawLine);
-            NotEnoughZombies.LOGGER.info(i + " " + line);
+            NotEnoughZombies.DEBUG_LOGGER.info(i + " " + rawLine);
+            NotEnoughZombies.DEBUG_LOGGER.info(i + " " + line);
 
         }
     }
