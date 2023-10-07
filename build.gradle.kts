4d19b96fa6da0cd14cd018aae0e0e529668a0201
PingIsFun <pingisfun@protonmail.com>
2023-10-07 19:05:01 +0200

    Add -dataCollector suffix to build jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ group = "cc.polyfrost"
 // Sets the name of the output jar (the one you put in your mods folder and send to other people)
 // It outputs all versions of the mod into the `build` directory.
 base {
-    archivesName.set("$mod_id-$platform")
+    archivesName.set("$mod_id-$platform-dataCollector")
 }
 
 // Configures the Polyfrost Loom, our plugin fork to easily set up the programming environment.
